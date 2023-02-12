90e2f73b620408698f48bfc670adc5c32ff4f6d6
KingRainbow44 <kobedo11@gmail.com>
2023-02-11 23:59:59 -0500

    Add recent users

--- src/features/social.ts
+++ src/features/social.ts
@@ -6,6 +6,7 @@ import type { BasicUser, OnlineUser } from "app/types";
 
 export let availableUsers: BasicUser[] = []; // Users which are online.
 export let onlineUsers: { [key: string]: OnlineUser } = {}; // Users which are actively using Laudiolin.
+export let recentUsers: { [key: string]: BasicUser } = {}; // Users which have been online recently.
 
 /* -------------------------------------------------- */
 
@@ -20,6 +21,17 @@ async function available(req: Request, rsp: Response): Promise<void> {
     }));
 }
 
+/**
+ * Fetches recently online users.
+ * @param req The HTTP request.
+ * @param rsp The new response.
+ */
+async function recent(req: Request, rsp: Response): Promise<void> {
+    rsp.status(200).send(constants.SUCCESS({
+        recentUsers: Object.values(recentUsers)
+    }));
+}
+
 /* -------------------------------------------------- */
 
 /* Create a router. */
@@ -27,6 +39,7 @@ const app: Router = Router();
 
 /* Configure routes. */
 app.get("/social/available", available);
+app.get("/social/recent", recent);
 
 /* Export the router. */
 export default app;
